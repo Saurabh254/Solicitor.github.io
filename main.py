ffa8463f3eb7e122e9f61d546a71025d9ff34413
Saurabh254 <sauravvishwakarma030@gmail.com>
2022-02-11 16:33:38 +0530

    makit ing

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     if message.author != bot.user:
-        if "saurabh" in message.content.lower().remove(":saurabh:"):
+        if "saurabh" in (message.content.lower()).remove(":saurabh:"):
             channel = bot.get_channel(941355481589485630)
             await channel.send(f"<@767758266155401226>\n```py\nMessage: {message.content}\n\nAuthor: {message.author}\n\nchannel: {message.channel}\n\nserver: {message.guild.name}\n```")
 bot.run("OTMyMTc0MTg0OTQyMDI2ODAy.YePI3A.hndJ0Hqpbn6QJcMMkFSBD0leaYw")
