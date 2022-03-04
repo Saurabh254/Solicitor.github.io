d9444ff764949c68d854c365f5ec19de1572467c
Saurabh254 <sauravvishwakarma030@gmail.com>
2022-03-04 14:08:25 +0530

    fixed everything

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 
 from discord.ext import commands
 import discord
-import datetime
-import os
-from decouple import config
+from datetime import datetime
 
-token = config("TOKEN")
-channel__id = config("channelid")
+import pytz
+
+token = "put token here"
+channel__id = "int: put channel id"
 
 bot = commands.Bot(command_prefix="s!")
-print(token, channel__id)
+
+IST = pytz.timezone('Asia/Kolkata')
+IndianTime = datetime.now(IST)
+
+
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Streaming(name="Messages", url="https://google.com/"))
@@ -17,18 +21,51 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    if message.author != bot.user:
+    if not message.author.bot:
         if "saurabh" in (message.content.lower()).replace(":saurabh:", ""):
             channel = bot.get_channel(channel__id)
-            embed = discord.Embed(title=f"Solicitor Headquarters!!!", color=16718362)
-            embed.add_field(name="Message", value=f"```py\n{message.content[0:1000]}\n```", inline=False)
-            embed.add_field(name="Author", value=f"```cpp\n{message.author}\n```", inline=True)
-            embed.add_field(name="Ping", value=f"```py\n💚 {round(bot.latency * 1000)}ms\n```", inline=True)
-            embed.add_field(name="Server Name", value=f"```py\n{message.guild.name}\n```", inline=True)
-            embed.add_field(name="Channel", value=f"```py\n{message.channel}\n```", inline=True)
-            embed.add_field(name="Message url:", value=f"[Jump to message]({message.jump_url})", inline=False)
-            embed.set_footer(text=str(datetime.datetime.now())[0:-7], icon_url="https://cdn.discordapp.com/attachments/941355481589485630/949172446387372043/images.jpg")
+
+            embed = discord.Embed(
+                title=f"Solicitor Headquarters!!!",
+                color=16718362)
+
+            embed.add_field(
+                name="Message",
+                value=f"```py\n{message.content[0:1000]}\n```",
+                inline=False)
+
+            embed.add_field(
+                name="Author",
+                value=f"```cpp\n{message.author}\n```",
+                inline=True)
+
+            embed.add_field(
+                name="Ping",
+                value=f"```py\n💚 {round(bot.latency * 1000)}ms\n```",
+                inline=True)
+
+            embed.add_field(
+                name="Server Name",
+                value=f"```py\n{message.guild.name}\n```",
+                inline=True)
+
+            embed.add_field(
+                name="Channel",
+                value=f"```py\n{message.channel}\n```",
+                inline=True)
+
+            embed.add_field(
+                name="Message URL:",
+                value=f"[Jump to message]({message.jump_url})",
+                inline=False)
+
+            embed.set_thumbnail(
+                url="https://cdn.discordapp.com/avatars/932174184942026802/5b33de427fa67237c3dc0dd58c4dcd3d.png?size=4096")
+
+            embed.set_footer(text=str(IndianTime.strftime('%d-%m-%Y %I:%M %p')),
+                             icon_url="https://cdn.discordapp.com/attachments/941355481589485630/949221082076938310/pinpng.com-timer-png-723861.png")
+
             await channel.send(embed=embed)
-            
+
 
 bot.run(token)
