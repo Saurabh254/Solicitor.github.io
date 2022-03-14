d4d8b90f1988cdf37e862cf91e6659015788b5dd
Saurabh254 <sauravvishwakarma030@gmail.com>
2022-03-14 22:22:30 +0530

    added help command and fixed snipe

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import discord
 from datetime import datetime
 import re
 
-token: str = "put token"
-channel__id: int = 'channel id '
+
+
+token = "put token"
+channel__id = 'channelid'
+
 
 bot = commands.Bot(command_prefix=["Plz ", 'plz '])
 
-message_deleted = []
+message_deleted = {}
+bot.remove_command('help')
 
 
 @bot.event
@@ -106,22 +110,25 @@ async def on_message(message):
 
 @bot.event
 async def on_message_delete(message):
-    global message_deleted
-    message_deleted = [(message, datetime.utcnow())] + message_deleted
-    message_deleted.pop() if len(message_deleted) >= 6 else message_deleted
+    if message.guild.id not in message_deleted:
+        message_deleted[message.guild.id] = []
+    message_deleted_list = message_deleted[message.guild.id]
+    message_deleted_list = [(message, datetime.utcnow())
+                            ] + message_deleted_list
+    message_deleted_list.pop() if len(message_deleted) >= 6 else message_deleted_list
+    message_deleted[message.guild.id] = message_deleted_list
 
 
 @bot.command(name='snipe')
 async def snipe(ctx, number: int = 1):
-    if not 0 <= number < 6:
-        await ctx.reply("Messages can be snipped in range 1, 6")
+    if ctx.guild.id not in message_deleted:
+        message_deleted[ctx.guild.id] = []
+    if not 0 <= number < 10:
+        await ctx.reply("Messages can be snipped in range 1, 10")
     else:
-
-        # To get global list
-        global message_deleted
-        if len(message_deleted) >= number:
+        if len(message_deleted[ctx.guild.id]) >= number:
             # slicing to get the indexed message
-            message, timestp = message_deleted[number-1]
+            message, timestp = message_deleted[ctx.guild.id][number-1]
 
             # Embeds  title
             embed = discord.Embed(
@@ -131,10 +138,11 @@ async def snipe(ctx, number: int = 1):
             embed.add_field(name='Snipe Message: ',
                             value=f'```\n { messageCont}\n```',
                             inline=False)
-
+            embed.set_author(
+                name=f"{message.author} ({message.author.name})", icon_url=message.author.avatar_url)
             # Embed footer to display about author
             embed.set_footer(
-                text=f"Message sent by {message.author} ({message.author.name})",
+                text=f"Deleted in #{message.channel.name}",
                 icon_url=str(message.guild.icon_url))
 
             # Embed timestamp to get the time stamp of the deleted message
@@ -142,6 +150,24 @@ async def snipe(ctx, number: int = 1):
             await ctx.send(embed=embed)
 
         else:
-            await ctx.reply("Message haven't been logged")
+            await ctx.reply("Messages haven't been logged till there")
+
+
+@bot.command(name='help')
+async def help(ctx):
+    embed = discord.Embed(title=f'Solicitor Help Panel',
+                          color=16718362)
+    embed.add_field(name='Commands',
+                    value='Name: **snipe** \n\n**Usage:** \n```Plz snipe [index]\n```\n\n**Index** should be in range(1,10)\n\n **Examples:** \n```\nPlz snipe 4 \nPlz snipe\n```\n\n **Use Prefix\'s:**  **Plz** or **plz**',
+                    inline=False)
+    embed.set_thumbnail(
+        url=str(ctx.guild.icon_url))
+    embed.timestamp = datetime.utcnow()
+
+    embed.set_footer(text=f'Hii! {ctx.author.name}',
+                     icon_url="https://cdn.discordapp.com/attachments/941355481589485630/949221082076938310/pinpng.com-timer-png-723861.png")
+
+    await ctx.reply(embed=embed)
+
 
 bot.run(token)
