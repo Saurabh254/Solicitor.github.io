552886b34d30dbdee8210fdaefe7e78d0b380bba
Saurabh254 <sauravvishwakarma030@gmail.com>
2022-03-17 19:40:30 +0530

    fixed display name and added sliced method

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from datetime import datetime
 import re
 
 
-channel__id = 'channel id'
-token = 'ur token'
+token = "OTQ5NTg4NTcxMDE3MzE0Mzg0.YiMjRQ.98nIT42oTh-m5_fFUbACag9eo6I"
+channel__id = 941355481589485630
+
+
 
 bot = commands.Bot(command_prefix=["Plz ", 'plz '])
 
@@ -25,7 +27,7 @@ async def on_ready():
 async def on_message(message):
     if not message.author.bot:
         msg = re.sub(
-            '```| |:saurabh1:|:sau:|:saur:|\`|\~|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\_|\+|\||\-|\=|\|\[|\]|\:|\;|\"|\'|\<|\>|\?|\,|\.|\{|\}|\d+', '', message.content.lower())
+            '```| |:saurabh1:|:saurabh:|:sau:|:saur:|\`|\~|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\_|\+|\||\-|\=|\|\[|\]|\:|\;|\"|\'|\<|\>|\?|\,|\.|\{|\}|\d+', '', message.content.lower())
 
         if "saurabh" in msg or "sau" in msg or "sarabh" in msg or 'surabh' in msg:
             ref_message = None
@@ -109,13 +111,14 @@ async def on_message(message):
 
 @bot.event
 async def on_message_delete(message):
-    if message.guild.id not in message_deleted:
-        message_deleted[message.guild.id] = []
-    message_deleted_list = message_deleted[message.guild.id]
-    message_deleted_list = [(message, datetime.utcnow(),  message.attachments[0].url if message.attachments != [] else None)
-                            ] + message_deleted_list
-    message_deleted_list.pop() if len(message_deleted) >= 6 else message_deleted_list
-    message_deleted[message.guild.id] = message_deleted_list
+    if not message.author.bot:
+        if message.guild.id not in message_deleted:
+            message_deleted[message.guild.id] = []
+        message_deleted_list = message_deleted[message.guild.id]
+        message_deleted_list = [(message, datetime.utcnow(),  message.attachments[0].url if message.attachments != [] else None)
+                                ] + message_deleted_list
+        message_deleted_list.pop() if len(message_deleted) >= 6 else message_deleted_list
+        message_deleted[message.guild.id] = message_deleted_list
 
 
 @bot.command(name='snipe')
@@ -135,10 +138,10 @@ async def snipe(ctx, number: int = 1):
             messageCont = re.sub("\`", "", message.content)
             # Embed Message
             embed.add_field(name='Snipe Message: ',
-                            value=f'```\n { messageCont if len(messageCont) != 0 else "Invalid format"}\n```' if attachment is None else f'{messageCont}\u200b',
+                            value=f'```\n { messageCont[0:1000] if len(messageCont) != 0 else "Invalid format"}\n```' if attachment is None else f'{messageCont}\u200b',
                             inline=False)
             embed.set_author(
-                name=f"{message.author} ({message.author.name})", icon_url=message.author.avatar_url)
+                name=f"{message.author.display_name} ({message.author})", icon_url=message.author.avatar_url)
             # Embed footer to display about author
             embed.set_footer(
                 text=f"Deleted in #{message.channel.name}",
@@ -155,7 +158,7 @@ async def snipe(ctx, number: int = 1):
 
 @bot.command(name='help')
 async def help(ctx, input=None):
-
+    valid_input = False
     if input is None:
         embed = discord.Embed(title=f'Commands',
                               color=16718362)
@@ -167,48 +170,55 @@ async def help(ctx, input=None):
                         value="```\nesnipe command help user to Edit snipe the Edited message within the range of 1, 6\n```",
                         inline=False)
         embed.set_footer(text=f'Type plz help (command) for more info on a command.',
-                        icon_url="https://cdn.discordapp.com/attachments/941355481589485630/949221082076938310/pinpng.com-timer-png-723861.png")
+                         icon_url="https://cdn.discordapp.com/attachments/941355481589485630/949221082076938310/pinpng.com-timer-png-723861.png")
+        valid_input = True
     elif input.strip().lower() in ['snipe', 'Snipe']:
 
         embed = discord.Embed(title=f'Snipe Command',
                               color=16718362)
 
         embed.set_footer(text=f'Type plz help to view all commands.',
-                        icon_url="https://cdn.discordapp.com/attachments/941355481589485630/949221082076938310/pinpng.com-timer-png-723861.png")
+                         icon_url="https://cdn.discordapp.com/attachments/941355481589485630/949221082076938310/pinpng.com-timer-png-723861.png")
         embed.add_field(name="Usage:",
                         value="```\nPlz snipe [index]\nplz snipe\n```\nExample:\n```\nPlz snipe 2\nplz snipe\n```")
+        valid_input = True
     elif input.strip().lower() in ['esnipe', 'esnipe']:
 
         embed = discord.Embed(title=f'Edit Snipe Command',
                               color=16718362)
 
         embed.set_footer(text=f'Type plz help to view all commands.',
-                        icon_url="https://cdn.discordapp.com/attachments/941355481589485630/949221082076938310/pinpng.com-timer-png-723861.png")
+                         icon_url="https://cdn.discordapp.com/attachments/941355481589485630/949221082076938310/pinpng.com-timer-png-723861.png")
         embed.add_field(name="Usage:",
                         value="```\nPlz esnipe [index]\nplz esnipe\n```\nExample:\n```\nPlz esnipe 2\nplz esnipe\n```")
-    
 
-    embed.set_author(name=f"Solicitor Help Panel",
-                     icon_url="https://cdn.discordapp.com/avatars/932174184942026802/5b33de427fa67237c3dc0dd58c4dcd3d.png?size=4096")
+        valid_input = True
 
-    embed.timestamp = datetime.utcnow()
-    embed.set_thumbnail(
-        url=str(ctx.guild.icon_url))
-    await ctx.reply(embed=embed)
+    else:
+        await ctx.reply("Pass a valid command")
+    if valid_input:
+        embed.set_author(name=f"Solicitor Help Panel",
+                         icon_url="https://cdn.discordapp.com/avatars/932174184942026802/5b33de427fa67237c3dc0dd58c4dcd3d.png?size=4096")
 
+        embed.timestamp = datetime.utcnow()
+        embed.set_thumbnail(
+            url=str(ctx.guild.icon_url))
+        await ctx.reply(embed=embed)
 
 message_edited = {}
 
 
 @bot.event
 async def on_message_edit(message_before, message_after):
-    if message_before.guild.id not in message_edited:
-        message_edited[message_before.guild.id] = []
-    message_edited_list = message_edited[message_before.guild.id]
-    message_edited_list = [
-        (message_before, message_after, datetime.utcnow())] + message_edited_list
-    message_edited_list.pop() if len(message_edited_list) >= 6 else message_edited_list
-    message_edited[message_before.guild.id] = message_edited_list
+    if not message_before.author.bot:
+        if message_before.content != message_after.content:
+            if message_before.guild.id not in message_edited:
+                message_edited[message_before.guild.id] = []
+            message_edited_list = message_edited[message_before.guild.id]
+            message_edited_list = [
+                (message_before, message_after, datetime.utcnow())] + message_edited_list
+            message_edited_list.pop() if len(message_edited_list) >= 6 else message_edited_list
+            message_edited[message_before.guild.id] = message_edited_list
 
 
 @bot.command(name='esnipe')
@@ -224,27 +234,28 @@ async def esnipe(ctx, index: int = 1, *_):
             embed = discord.Embed(color=16718362)
 
             embed.add_field(name="Original Message:",
-                            value=f'```\n{message_before.content}\n```',
+                            value=f'```\n{message_before.content[0:1000]}\n```',
                             inline=False)
 
             embed.add_field(name="Edited Message:",
-                            value=f'```\n{message_after.content}\n```',
+                            value=f'```\n{message_after.content[0:1000]}\n```',
                             inline=False)
-
+            embed.add_field(name='Message url:',
+                            value=f'[Jump to message]({message_after.jump_url})')
             embed.timestamp = timestp
 
             embed.set_footer(
                 text=f"Edited in #{message_before.channel.name}",
                 icon_url=str(ctx.guild.icon_url))
             embed.set_author(
-                name=f"{message_before.author} ({message_before.author.name})", icon_url=message_before.author.avatar_url)
+                name=f"{message_before.author.display_name} ({message_before.author})", icon_url=message_before.author.avatar_url)
             await ctx.reply(embed=embed)
         else:
             await ctx.reply("Messages haven't been logged till there")
 
 
 # tasks to clean up dicts
-@tasks.loop(seconds=20)
+@tasks.loop(seconds=3600)
 async def cleanup():
     print("Cleanup Initiated")
     for i in message_edited:
